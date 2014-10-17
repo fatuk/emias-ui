67ad0f293fd4f63a6826d867bd8fae961e4bea49
fatuk <fatukk@gmail.com>
2014-10-17 11:10:55 +0400

    Add sass source map

--- grunt/autoprefixer.js
+++ grunt/autoprefixer.js
@@ -1,6 +1,7 @@
 module.exports = {
     autoprefix: {
         options: {
+            map: true,
             browsers: ['Android 2', 'ie 8', 'ie 9']
         },
         src: 'css/main.css'
--- grunt/sass.js
+++ grunt/sass.js
@@ -1,6 +1,6 @@
 module.exports = {
     options: {
-        sourceMap: true
+        style: 'compressed'
     },
     dist: {
         files: {
